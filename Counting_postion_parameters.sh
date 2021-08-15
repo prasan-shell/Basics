89a3450e405dadf91f67c3d43fcc47b57eca2a3a
prasancto <prasancto@gmail.com>
2021-08-15 04:41:52 +0000

    Adding the count script

---
+++ Counting_postion_parameters.sh
@@ -0,0 +1,15 @@
+
+######################################################
+##  Purpose: Counting positional parameters          ##
+##  Version: 1.0                                     ##
+##  Author: Prasan                                   ##
+##  Run: ./Counting_postion_parameters.sh hello in   ##    
+#######################################################
+
+#!/bin/bash
+
+ ### Start of script ##### 
+ 
+ echo "Print the number of parameters given : $#"
+
+ ### End of Script   ##### 
